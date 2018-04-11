87da7fefe27ed87ba420de355580e72261ebbc6e
Diana Bank <bankbits@dhcp-mcfad-3208.redrover.cornell.edu>
2018-04-10 20:21:08 -0400

    sexing things up

---
+++ jsons/clean_data.py
@@ -0,0 +1,90 @@
+import json
+import re
+
+with open('alcoholic_cocktails_webtender_ingredients.json', 'r') as fr:
+	data_dict = json.load(fr)
+
+new_dict = {}
+
+'''
+	CONVERSION FACTORS:
+	1 part = 1.5 oz
+	1 centiliter (cl) = 0.33814 oz
+	1 shot = 1.5 oz
+'''
+
+for cock in data_dict:
+	recipe = []
+
+	for ingredient in data_dict[cock]:
+
+		ing = ingredient[1].encode('utf-8')
+		ing = str(ing)
+		ing = " ".join(ing.split())
+		
+		amt = re.findall(r"[-+]?\d*\.\d+|\d+", ing)
+		amt = [float(s) for s in amt]
+		print(amt)
+
+		val = 0
+
+		if(len(amt) == 0): 
+			recipe.append(ingredient)
+			continue
+
+		elif len(amt) == 1: val = amt[0]
+		elif len(amt) == 2: val = amt[0] / amt[1]
+		elif len(amt) == 3: val = amt[0] + (amt[1] / amt[2])
+		
+
+		if "part" in ingredient[1]:
+
+			val *= 1.5
+			recipe.append([ingredient[0], str(val) + " oz"])
+
+		elif "cl" in ingredient[1]:
+
+			val *= 0.33814
+			recipe.append([ingredient[0], str(val) + " oz"])
+
+		elif "shot" in ingredient[1]:
+
+			val *= 1.5
+			recipe.append([ingredient[0], str(val) + " oz"])
+
+		elif " L " in ingredient[1]:
+			val *= 33.814
+			recipe.append([ingredient[0], str(val) + " oz"])
+
+		else:
+			recipe.append(ingredient)
+
+	new_dict[cock] = recipe
+
+'''
+if len(amt) == 1: val = float(amt[0])
+elif len(amt) == 2: val = float(amt[0]) / float(amt[1])
+elif len(amt) == 3: val = float(amt[0]) + (float(amt[1]) / float(amt[2]))
+else: val == None
+
+
+if val != None:
+
+
+	
+
+else:
+	
+	recipe.append(ingredient)
+
+
+
+'''
+
+
+
+data = json.dumps(new_dict, indent=4, separators=(',', ': '))
+with open("clean_data.json", "w") as fw:
+	fw.write(data)
+
+
